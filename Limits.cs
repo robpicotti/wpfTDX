6218e7cefac32ab1006b1156a7a024abe3087705
rpicotti <robpicotti@gmail.com>
2024-04-15 14:55:24 +0200

    adding fund limits funct

--- Limits.cs
+++ Limits.cs
@@ -9,11 +9,26 @@ using TDX;
 
 namespace wpfTDX
 {
+    public enum LimitType
+    {
+        Weight,
+        StockNotionalPct,
+        FuturesNotionalPct,
+        Liquidity,
+        StockLeverage,
+        FuturesLeverage,
+        Leverage,
+        VarLimitFactor,
+        StressLimitFactor,
+        Drawdown
+    }
+
     public class Limits
     {
        public SqlConnection gbl_conn { get; set; }
        public FundLimits fundLimits { get; set; }
        public List<string> lstFundLimits = new List<string>();
+
         public Limits(string fundName, SqlConnection conn)
         {
             this.gbl_conn = conn;
@@ -38,7 +53,9 @@ namespace wpfTDX
             lstFundLimits.Add("drawdown_limit");
             return lstFundLimits;
         }
+
     }
+
     class WeightLimits
     {
 
@@ -67,156 +84,195 @@ namespace wpfTDX
         private DataTable dtFundData { get; set; }
         private DataTable dtFundLimits { get; set; }
         private DataTable dtLive { get; set; }
+        private db DB = new db();
         public FundLimits(string fundname, SqlConnection conn)
         {
-            this.fundname = fundname;
-            this.gbl_conn = conn;
-            this.stk_notional_pct_limit = new StockNotionalPctLimit();
-            this.fut_notional_pct_limit = new FuturesNotionalPctLimit();
-            this.stk_leverage_limit = new StockLeverageLimit();
-            this.fut_leverage_limit = new FuturesLeverageLimit();
-            this.leverage_limit = new LeverageLimit();
-            this.var_limit_factor = new VarLimitFactor();
-            this.stress_limit_factor = new StressLimitFactor();
-            this.drawdown_limit = new DrawDownLimit();
-            this.liquidity_limit = new LiquidityLimit();
-            this.weight_limit = new WeightLimit();
-            //get the default data from the funds table
-            tblFunds = new Table("funds", this.gbl_conn, "WHERE fundname='" + this.fundname + "'");
-            dtFundData = new DataTable();
-            dtFundData = tblFunds.table_data;
-            // get the custom data from the funds_limit table
-            tblFundLimits = new Table("fund_limits", this.gbl_conn, "WHERE fundname='" + fundname + "'");
-            dtFundLimits = tblFundLimits.table_data;
-            //get the live limits data from the postions_target table
-            tblTargetPositions = new Table("target_positions", this.gbl_conn, "WHERE fundname='" + fundname + "'");
-            dtLive = tblTargetPositions.table_data;
-
-            if(dtFundLimits.Rows.Count>0)
+            try
             {
-                string enabled = dtFundLimits.Rows[0]["enabled"].ToString();
-                if (enabled == "1")
+                this.fundname = fundname;
+                this.gbl_conn = conn;
+                this.stk_notional_pct_limit = new StockNotionalPctLimit();
+                this.fut_notional_pct_limit = new FuturesNotionalPctLimit();
+                this.stk_leverage_limit = new StockLeverageLimit();
+                this.fut_leverage_limit = new FuturesLeverageLimit();
+                this.leverage_limit = new LeverageLimit();
+                this.var_limit_factor = new VarLimitFactor();
+                this.stress_limit_factor = new StressLimitFactor();
+                this.drawdown_limit = new DrawDownLimit();
+                this.liquidity_limit = new LiquidityLimit();
+                this.weight_limit = new WeightLimit();
+                //get the default data from the funds table
+                tblFunds = new Table("funds", this.gbl_conn, "WHERE fundname='" + this.fundname + "'");
+                dtFundData = new DataTable();
+                dtFundData = tblFunds.table_data;
+                // get the custom data from the funds_limit table
+                tblFundLimits = new Table("fund_limits", this.gbl_conn, "WHERE fundname='" + fundname + "'");
+                dtFundLimits = tblFundLimits.table_data;
+                //get the live limits data from the postions_target table
+                tblTargetPositions = new Table("target_positions", this.gbl_conn, "WHERE fundname='" + fundname + "'");
+                dtLive = tblTargetPositions.table_data;
+
+                if (dtFundLimits.Rows.Count > 0)
                 {
-                    string strValue = dtFundLimits.Rows[0]["stk_leverage_limit"].ToString();
+                    bool enabled = dtFundLimits.Rows[0]["enabled"] != null ? Convert.ToBoolean(dtFundLimits.Rows[0]["enabled"]) : false;
+
+                    if (enabled)
+                    {
+                        string strValue = dtFundLimits.Rows[0]["stk_leverage_limit"].ToString();
+                        double parsedOut;
+                        if (Double.TryParse(strValue, out parsedOut))
+                        {
+                            this.stk_leverage_limit.customValue = parsedOut;
+                        }
+                        strValue = dtFundLimits.Rows[0]["weight_limit"].ToString();
+                        if (Double.TryParse(strValue, out parsedOut))
+                        {
+                            this.weight_limit.customValue = parsedOut;
+                        }
+                        strValue = dtFundLimits.Rows[0]["stk_notional_pct_limit"].ToString();
+                        if (Double.TryParse(strValue, out parsedOut))
+                        {
+                            this.stk_notional_pct_limit.customValue = parsedOut;
+                        }
+                        strValue = dtFundLimits.Rows[0]["fut_notional_pct_limit"].ToString();
+                        if (Double.TryParse(strValue, out parsedOut))
+                        {
+                            this.fut_notional_pct_limit.customValue = parsedOut;
+                        }
+                        strValue = dtFundLimits.Rows[0]["liquidity_limit"].ToString();
+                        if (Double.TryParse(strValue, out parsedOut))
+                        {
+                            this.liquidity_limit.customValue = parsedOut;
+                        }
+                        strValue = dtFundLimits.Rows[0]["stk_leverage_limit"].ToString();
+                        if (Double.TryParse(strValue, out parsedOut))
+                        {
+                            this.stk_leverage_limit.customValue = parsedOut;
+                        }
+                        strValue = dtFundLimits.Rows[0]["fut_leverage_limit"].ToString();
+                        if (Double.TryParse(strValue, out parsedOut))
+                        {
+                            this.fut_leverage_limit.customValue = parsedOut;
+                        }
+                        strValue = dtFundLimits.Rows[0]["leverage_limit"].ToString();
+                        if (Double.TryParse(strValue, out parsedOut))
+                        {
+                            this.leverage_limit.customValue = parsedOut;
+                        }
+                        strValue = dtFundLimits.Rows[0]["var_limit_factor"].ToString();
+                        if (Double.TryParse(strValue, out parsedOut))
+                        {
+                            this.var_limit_factor.customValue = parsedOut;
+                        }
+                        strValue = dtFundLimits.Rows[0]["stress_limit_factor"].ToString();
+                        if (Double.TryParse(strValue, out parsedOut))
+                        {
+                            this.stress_limit_factor.customValue = parsedOut;
+                        }
+                        strValue = dtFundLimits.Rows[0]["drawdown_limit"].ToString();
+                        if (Double.TryParse(strValue, out parsedOut))
+                        {
+                            this.drawdown_limit.customValue = parsedOut;
+                        }
+                        strValue = dtFundLimits.Rows[0]["drawdown_limit"].ToString();
+                        if (Double.TryParse(strValue, out parsedOut))
+                        {
+                            this.drawdown_limit.customValue = parsedOut;
+                        }
+                    }
+                }
+                if (dtFundData.Rows.Count > 0)
+                {
+                    string stkLeverageLimitDefault = dtFundData.Rows[0]["stk_leverage_limit"].ToString();
                     double parsedOut;
-                    if (Double.TryParse(strValue, out parsedOut))
+                    if (Double.TryParse(stkLeverageLimitDefault, out parsedOut))
                     {
-                        this.stk_leverage_limit.customValue = parsedOut;
+                        this.stk_leverage_limit.defaultValue = parsedOut;
                     }
-                    strValue = dtFundLimits.Rows[0]["weight_limit"].ToString();
-                    if (Double.TryParse(strValue, out parsedOut))
+                    string futLevLimit = dtFundData.Rows[0]["fut_leverage_limit"].ToString();
+                    if (Double.TryParse(futLevLimit, out parsedOut))
                     {
-                        this.weight_limit.customValue = parsedOut;
+                        this.fut_leverage_limit.defaultValue = parsedOut;
                     }
-                    strValue = dtFundLimits.Rows[0]["stk_notional_pct_limit"].ToString();
-                    if (Double.TryParse(strValue, out parsedOut))
+                    string leverageLimit = dtFundData.Rows[0]["leverage_limit"].ToString();
+                    if (Double.TryParse(leverageLimit, out parsedOut))
                     {
-                        this.stk_notional_pct_limit.customValue = parsedOut;
+                        this.leverage_limit.defaultValue = parsedOut;
                     }
-                    strValue = dtFundLimits.Rows[0]["fut_notional_pct_limit"].ToString();
-                    if (Double.TryParse(strValue, out parsedOut))
+                    string varlimitfactor = dtFundData.Rows[0]["var_limit_factor"].ToString();
+                    if (Double.TryParse(varlimitfactor, out parsedOut))
                     {
-                        this.fut_notional_pct_limit.customValue = parsedOut;
+                        this.var_limit_factor.defaultValue = parsedOut;
                     }
-                    strValue = dtFundLimits.Rows[0]["liquidity_limit"].ToString();
-                    if (Double.TryParse(strValue, out parsedOut))
+                    string stressLimitFactor = dtFundData.Rows[0]["stress_limit_factor"].ToString();
+                    if (Double.TryParse(stressLimitFactor, out parsedOut))
                     {
-                        this.liquidity_limit.customValue = parsedOut;
+                        this.stress_limit_factor.defaultValue = parsedOut;
                     }
-                    strValue = dtFundLimits.Rows[0]["stk_leverage_limit"].ToString();
-                    if (Double.TryParse(strValue, out parsedOut))
+                    string drawDownLimit = dtFundData.Rows[0]["drawdown_limit"].ToString();
+                    if (Double.TryParse(drawDownLimit, out parsedOut))
                     {
-                        this.stk_leverage_limit.customValue = parsedOut;
+                        this.drawdown_limit.defaultValue = parsedOut;
                     }
-                    strValue = dtFundLimits.Rows[0]["fut_leverage_limit"].ToString();
-                    if (Double.TryParse(strValue, out parsedOut))
+                    string stkNotionalPctLimit = dtFundData.Rows[0]["stk_notional_pct_limit"].ToString();
+                    if (Double.TryParse(stkNotionalPctLimit, out parsedOut))
                     {
-                        this.fut_leverage_limit.customValue = parsedOut;
+                        this.stk_notional_pct_limit.defaultValue = parsedOut;
                     }
-                    strValue = dtFundLimits.Rows[0]["leverage_limit"].ToString();
-                    if (Double.TryParse(strValue, out parsedOut))
+                    string futNotionalPctLimit = dtFundData.Rows[0]["fut_notional_pct_limit"].ToString();
+                    if (Double.TryParse(futNotionalPctLimit, out parsedOut))
                     {
-                        this.leverage_limit.customValue = parsedOut;
+                        this.fut_notional_pct_limit.defaultValue = parsedOut;
                     }
-                    strValue = dtFundLimits.Rows[0]["var_limit_factor"].ToString();
-                    if (Double.TryParse(strValue, out parsedOut))
+                    string liquidityLimit = dtFundData.Rows[0]["liquidity_limit"].ToString();
+                    if (Double.TryParse(liquidityLimit, out parsedOut))
                     {
-                        this.var_limit_factor.customValue = parsedOut;
+                        this.liquidity_limit.defaultValue = parsedOut;
                     }
-                    strValue = dtFundLimits.Rows[0]["stress_limit_factor"].ToString();
-                    if (Double.TryParse(strValue, out parsedOut))
+                    string weight_limit = dtFundData.Rows[0]["weight_limit"].ToString();
+                    if (Double.TryParse(weight_limit, out parsedOut))
                     {
-                        this.stress_limit_factor.customValue = parsedOut;
-                    }
-                    strValue = dtFundLimits.Rows[0]["drawdown_limit"].ToString();
-                    if (Double.TryParse(strValue, out parsedOut))
-                    {
-                        this.drawdown_limit.customValue = parsedOut;
-                    }
-                    strValue = dtFundLimits.Rows[0]["drawdown_limit"].ToString();
-                    if (Double.TryParse(strValue, out parsedOut))
-                    {
-                        this.drawdown_limit.customValue = parsedOut;
+                        this.weight_limit.defaultValue = parsedOut;
                     }
                 }
             }
-            if(dtFundData.Rows.Count>0)
+            catch(Exception ex)
             {
-                string stkLeverageLimitDefault = dtFundData.Rows[0]["stk_leverage_limit"].ToString();
-                double parsedOut;
-                if(Double.TryParse(stkLeverageLimitDefault,out parsedOut))
-                {
-                    this.stk_leverage_limit.defaultValue = parsedOut;
-                }
-                string futLevLimit = dtFundData.Rows[0]["fut_leverage_limit"].ToString();
-                if(Double.TryParse(futLevLimit,out parsedOut))
-                {
-                    this.fut_leverage_limit.defaultValue = parsedOut;
-                }
-                string leverageLimit = dtFundData.Rows[0]["leverage_limit"].ToString();
-                if (Double.TryParse(leverageLimit, out parsedOut))
-                {
-                    this.leverage_limit.defaultValue = parsedOut;
-                }
-                string varlimitfactor = dtFundData.Rows[0]["var_limit_factor"].ToString();
-                if (Double.TryParse(varlimitfactor, out parsedOut))
-                {
-                    this.var_limit_factor.defaultValue = parsedOut;
-                }
-                string stressLimitFactor = dtFundData.Rows[0]["stress_limit_factor"].ToString();
-                if (Double.TryParse(stressLimitFactor, out parsedOut))
-                {
-                    this.stress_limit_factor.defaultValue = parsedOut;
-                }
-                string drawDownLimit = dtFundData.Rows[0]["drawdown_limit"].ToString();
-                if (Double.TryParse(drawDownLimit, out parsedOut))
-                {
-                    this.drawdown_limit.defaultValue = parsedOut;
-                }
-                string stkNotionalPctLimit = dtFundData.Rows[0]["stk_notional_pct_limit"].ToString();
-                if (Double.TryParse(stkNotionalPctLimit, out parsedOut))
-                {
-                    this.stk_notional_pct_limit.defaultValue = parsedOut;
-                }
-                string futNotionalPctLimit = dtFundData.Rows[0]["fut_notional_pct_limit"].ToString();
-                if(Double.TryParse(futNotionalPctLimit,out parsedOut))
-                {
-                    this.fut_notional_pct_limit.defaultValue = parsedOut;
-                }
-                string liquidityLimit = dtFundData.Rows[0]["liquidity_limit"].ToString();
-                if (Double.TryParse(liquidityLimit, out parsedOut))
-                {
-                    this.liquidity_limit.defaultValue = parsedOut;
-                }
-                string weight_limit = dtFundData.Rows[0]["weight_limit"].ToString();
-                if(Double.TryParse(weight_limit,out parsedOut))
-                {
-                    this.weight_limit.defaultValue = parsedOut;
-                }
+                throw new Exception("FundLimits exception: " + ex.Message);
             }
-
         }
+
+        public void UpdateFundLimits(string action)
+        {
+            string now = DateTime.UtcNow.ToString("dd-MMM-yyyy hh:mm:ss");
+            Table tbl = new Table("fund_limits", this.gbl_conn, select_data: false);
+            List<string> fund_limits_columns = tbl.get_table_columns();
+            string insertSQL = "INSERT fund_limits(";
+            for (int i = 0; i < fund_limits_columns.Count; i++)
+            {
+                insertSQL += fund_limits_columns[i].ToString() + ",";
+            }
+            insertSQL = insertSQL.Substring(0, insertSQL.Length - 1);
+            insertSQL += ") VALUES('" + now + "','" + this.fundname + "',";
+            insertSQL += (this.weight_limit.customValue != null ? this.weight_limit.customValue.ToString() : "NULL") + ",";
+            insertSQL += (this.stk_notional_pct_limit.customValue != null ? this.stk_notional_pct_limit.customValue.ToString(): "NULL") + ",";
+            insertSQL += (this.fut_notional_pct_limit.customValue != null ? this.fut_notional_pct_limit.customValue.ToString(): "NULL" )+ ",";
+            insertSQL += (this.liquidity_limit.customValue != null ? this.liquidity_limit.customValue.ToString(): "NULL" ) + ",";
+            insertSQL += (this.stk_leverage_limit.customValue != null ? this.stk_leverage_limit.customValue.ToString(): "NULL") + ",";
+            insertSQL += (this.fut_leverage_limit.customValue != null ? this.fut_leverage_limit.customValue.ToString(): "NULL" ) + ",";
+            insertSQL += (this.leverage_limit.customValue !=null ? this.leverage_limit.customValue.ToString() :"NULL") + ",";
+            insertSQL += (this.var_limit_factor.customValue != null ? this.var_limit_factor.customValue.ToString() : "NULL") + ",";
+            insertSQL += (this.stress_limit_factor.customValue != null ? this.stress_limit_factor.customValue.ToString() :"NULL") + ",";
+            insertSQL += (this.drawdown_limit.customValue != null ? this.drawdown_limit.customValue.ToString(): "NULL") + ",";
+            insertSQL += "1,'"; //set enabled to true
+            insertSQL += action + "')";
+            //insert data to fund_limits table
+            DB.execSQL_noresults(insertSQL, this.gbl_conn);
+        }
+
     }
+
+
 
     public  class StockLeverageLimit
     {
--- ucLimits.xaml.cs
+++ ucLimits.xaml.cs
@@ -25,7 +25,9 @@ namespace wpfTDX
         public event EventHandler RemoveControlRequested;
         public SqlConnection gbl_conn;
         DataTable dtFunds;
+        Limits limits;
         string FundName;
+        bool blnValueChanged = false;
         TDX.db _db = new TDX.db();
         public ucLimits(SqlConnection conn)
         {
@@ -46,8 +48,10 @@ namespace wpfTDX
         }
         private void cboFundName_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
+            this.blnValueChanged = false;
+            btnUpdateCustom.Visibility = Visibility.Hidden;
             this.FundName = cboFundName.SelectedValue.ToString();
-            Limits limits = new Limits(this.FundName, this.gbl_conn);
+            this.limits = new Limits(this.FundName, this.gbl_conn);
             DataTable dtAll = new DataTable();
             dtAll.Columns.Add("metric");
             dtAll.Columns.Add("default");
@@ -109,6 +113,120 @@ namespace wpfTDX
         private void cmdClose_Click(object sender, RoutedEventArgs e)
         {
             RemoveControlRequested?.Invoke(this, EventArgs.Empty);
+        }
+
+        private void btnUpdateCustom_Click(object sender, RoutedEventArgs e)
+        {
+            string action = (cboAction.SelectedItem as ComboBoxItem)?.Content.ToString();
+            if (this.blnValueChanged && action !="")
+            {
+                try
+                {
+                    this.limits.fundLimits.UpdateFundLimits(action);
+                    MessageBox.Show("fund_limits updated", "fund limits", MessageBoxButton.OK, MessageBoxImage.Information);
+                }
+                catch(Exception ex)
+                {
+                    MessageBox.Show(ex.Message, "update fund limits", MessageBoxButton.OK, MessageBoxImage.Error);
+                }
+            }
+        }
+
+        private void dgFundLimits_CellEditEnding(object sender, DataGridCellEditEndingEventArgs e)
+        {
+            try
+            {
+                if (e.EditAction == DataGridEditAction.Commit)
+                {
+                    this.blnValueChanged = true;
+                    btnUpdateCustom.Visibility = Visibility.Visible;
+                    // Get the edited cell's row index
+                    int rowIndex = e.Row.GetIndex();
+
+                    // Get the corresponding data item
+                    if (dgFundLimits.Items[rowIndex] is DataRowView rowView)
+                    {
+                        // Access the first column's value
+                        var valueOfFirstColumn = rowView[0]; // Assuming the first column's index is 0
+                        var valueOfSecondColumn = rowView[1];
+                        var editedValueAfter = (e.EditingElement as TextBox).Text;
+                        var valueOfFourthColumn = rowView[3];
+                        // Convert to string if necessary
+                        string stringValueOfFirstColumn = valueOfFirstColumn.ToString();
+                        string stringValueOfSecondColumn = valueOfSecondColumn.ToString();
+                        string stringValueOfThirdColumn = (editedValueAfter.ToString() !="" ? editedValueAfter.ToString() : "NULL") ;
+                        string stringValueOfFourthColumn = valueOfFourthColumn.ToString();
+                        if (e.Column.DisplayIndex == 2)
+                        {
+                            double? parsedValue;
+                            if (double.TryParse(stringValueOfThirdColumn, out double result))
+                            {
+                                parsedValue = result;
+                            }
+                            else
+                            {
+                                parsedValue =null; // or any other default value you prefer
+                            }
+                            switch (stringValueOfFirstColumn)
+                            {
+                                case "weight_limit":
+                                    this.limits.fundLimits.weight_limit.customValue = parsedValue;
+                                    break;
+                                case "stk_notional_pct_limit":
+                                    this.limits.fundLimits.stk_notional_pct_limit.customValue = parsedValue;
+                                    break;
+                                case "fut_notional_pct_limit":
+                                    this.limits.fundLimits.fut_notional_pct_limit.customValue = parsedValue;
+                                    break;
+                                case "liquidity_limit":
+                                    this.limits.fundLimits.liquidity_limit.customValue = parsedValue;
+                                    break;
+                                case "stk_leverage_limit":
+                                    this.limits.fundLimits.stk_leverage_limit.customValue = parsedValue;
+                                    break;
+                                case "fut_leverage_limit":
+                                    this.limits.fundLimits.fut_leverage_limit.customValue = parsedValue;
+                                    break;
+                                case "leverage_limit":
+                                    this.limits.fundLimits.leverage_limit.customValue = parsedValue;
+                                    break;
+                                case "var_limit_factor":
+                                    this.limits.fundLimits.var_limit_factor.customValue = parsedValue;
+                                    break;
+                                case "stress_limit_factor":
+                                    this.limits.fundLimits.stress_limit_factor.customValue = parsedValue;
+                                    break;
+                                case "drawdown_limit":
+                                    this.limits.fundLimits.drawdown_limit.customValue = parsedValue;
+                                    break;
+                            }
+                        }
+                        else if (e.Column.DisplayIndex == 0)
+                        {
+                            ((TextBox)e.EditingElement).Text = stringValueOfFirstColumn;
+                            e.Cancel = true;
+                            MessageBox.Show("You can only edit the custom value field", "edit fund limits", MessageBoxButton.OK, MessageBoxImage.Error);
+                        }
+                        else if (e.Column.DisplayIndex == 1)
+                        {
+                            ((TextBox)e.EditingElement).Text = stringValueOfSecondColumn;
+                            e.Cancel = true;
+                            MessageBox.Show("You can only edit the custom value field", "edit fund limits", MessageBoxButton.OK, MessageBoxImage.Error);
+                        }
+                        else if (e.Column.DisplayIndex == 3)
+                        {
+                            ((TextBox)e.EditingElement).Text = stringValueOfFourthColumn;
+                            e.Cancel = true;
+                            MessageBox.Show("You can only edit the custom value field", "edit fund limits", MessageBoxButton.OK, MessageBoxImage.Error);
+                        }
+                    }
+                }
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show(ex.Message, "update fund limits", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+
         }
     }
 }
