5de1b415f11f1b6c669a0dfbf25f7b0b6b2469a0
rpicotti <robpicotti@gmail.com>
2025-08-06 16:52:41 +0100

    started datamodel classes

--- DataModels/TadPositionsDataModel.cs
+++ DataModels/TadPositionsDataModel.cs
@@ -24,8 +24,99 @@ namespace wpfTDX
             get => _tickername;
             set
             {
-                if()
+                if(value != _tickername)
+                {
+                    _tickername = value;
+                    OnPropertyChanged(nameof(Tickername));
+                }
             }
         }
+        private string _subtickername;
+        [JsonProperty("sub_tickername")]
+        public string SubTickername
+        {
+            get => _subtickername;
+            set
+            {
+                if(value != _subtickername)
+                {
+                    _subtickername = value;
+                    OnPropertyChanged(nameof(SubTickername));
+                }
+            }
+        }
+        private string _currentcontract;
+        [JsonProperty("current_contract")]
+        public string CurrentContract
+        {
+            get => _currentcontract;
+            set
+            {
+                if(value != _currentcontract)
+                {
+                    _currentcontract = value;
+                    OnPropertyChanged(nameof(CurrentContract));
+                }
+            }
+        }
+        private string _tadid;
+        [JsonProperty("tad_id")]
+        public string TadId
+        {
+            get => _tadid;
+            set
+            {
+                if(value != _tadid)
+                {
+                    _tadid = value;
+                    OnPropertyChanged(nameof(TadId));
+                }
+            }
+        }
+
+        private string _bbgsymbol;
+        [JsonProperty("bbg_symbol")]
+        public string BbgSymbol
+        {
+            get => _bbgsymbol;
+            set
+            {
+                if(value != _bbgsymbol)
+                {
+                    _bbgsymbol = value;
+                    OnPropertyChanged(nameof(BbgSymbol));
+                }
+            }
+        }
+
+        private string _rolldate;
+        [JsonProperty("roll_date")]
+        public string Rolldate
+        {
+            get => _rolldate;
+            set
+            {
+                if(value != _rolldate)
+                {
+                    _rolldate = value;
+                    OnPropertyChanged(nameof(Rolldate));
+                }
+            }
+        }
+        private float _view_position_base_D1;
+        [JsonProperty("view_position_base_D1")]
+        public float ViewPositionBaseD1
+        {
+            get => _view_position_base_D1;
+            set
+            {
+                if(value != _view_position_base_D1)
+                {
+                    _view_position_base_D1 = value;
+                    OnPropertyChanged(nameof(ViewPositionBaseD1));
+                }
+            }
+        }
+
     }
 }
