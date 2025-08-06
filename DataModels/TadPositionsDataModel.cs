aa3c054eb46f2e3631d3634b238378abd40eb6a7
rpicotti <robpicotti@gmail.com>
2025-08-06 16:20:11 +0100

    test

---
+++ DataModels/TadPositionsDataModel.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using Newtonsoft.Json;
+
+namespace wpfTDX
+{
+    public class TadPositionsDataModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public void OnPropertyChanged([CallerMemberName] string name = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+        }
+        private string _tickername;
+        [JsonProperty("tickername")]
+        public string Tickername
+        {
+            get => _tickername;
+            set
+            {
+                if()
+            }
+        }
+    }
+}
